809276a4c1eef3a02f392ff087d92a55712cf3a0
Ryan Lewis <accounts+github@ryandlewis.dev>
2023-12-08 12:41:24 -0600

    Breakout name and version scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,12 @@
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cd $SCRIPTPATH/..
+version=`$SCRIPTPATH/version.sh`
+name=`$SCRIPTPATH/name.sh`
 
-version=`grep -oP '(?<=version = ")[^"]+' pyproject.toml | head -n 1`
-name=`grep -oP '(?<=name = ")[^"]+' pyproject.toml | head -n 1`
+cd $SCRIPTPATH/..
 
 docker build \
 	-t ghcr.io/ad-sdl/${name}:${version} \
 	-t ghcr.io/ad-sdl/${name}:dev \
 	.
-
